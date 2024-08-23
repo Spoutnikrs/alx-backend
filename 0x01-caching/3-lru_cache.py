77c400b513913f0e18739fd90dd9325b233ac754
rayesyounes <rayesyounes88@gmail.com>
2024-08-23 22:56:25 +0100

    refactor lru

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -10,27 +10,31 @@ class LRUCache(BaseCaching):
     """
 
     def __init__(self):
+        """
+        initialize the LRUCache
+        """
         super().__init__()
         self.order = []
 
     def put(self, key, item):
-
-        if key and item:
+        """
+        Add an item in the cache
+        """
+        if key is not None and item is not None:
             if len(self.cache_data) >= self.MAX_ITEMS:
                 discarded_key = self.order.pop(0)
                 del self.cache_data[discarded_key]
-                print(f"DISCARD: {discarded_key}")
+                print("DISCARD:", discarded_key)
 
             self.cache_data[key] = item
-            self._update_order(key)
+            self.order.append(key)
 
     def get(self, key):
+        """
+        Get an item by key
+        """
         if key in self.cache_data:
-            self._update_order(key)
+            self.order.remove(key)
+            self.order.append(key)
             return self.cache_data[key]
         return None
-
-    def _update_order(self, key):
-        if key in self.order:
-            self.order.remove(key)
-        self.order.append(key)
